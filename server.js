bcb9540179a0c04459cecfc9bebc9f3d9e423fa6
jianthe1 <136588615+jianthe1@users.noreply.github.com>
2026-04-02 23:44:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,4 +10,12 @@ app.get('/', async (req, res) => {
     res.send(`Hello! This page has been viewed ${count} times.`);
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.get('/readyz', (req, res) => {
+  res.status(200).send('OK');
+});
+
 app.listen(3000, () => console.log('Server running on port 3000'));
