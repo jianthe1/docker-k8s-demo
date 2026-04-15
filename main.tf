a1cc58abf838382cf5a290d6a010aaa04d2a5b78
Cloud Architect <jian88533@gmail.com>
2026-04-15 18:28:30 +0800

    Lets try again

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "region" {}
-variable "private_key_path" {
+variable "private_key" {
   sensitive = true
 }
 
@@ -17,7 +17,7 @@ provider "oci" {
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   region           = var.region
-  # private_key_path = var.private_key_path
+  private_key  = var.private_key
 }
 
 # --- 3. THE HANDSHAKE (Ask Oracle for a list of Data Centers) ---
